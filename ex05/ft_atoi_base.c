ee1f678dfdd01bfee52dea0d063aabebf54c1cb3
mrbluebin <43376896+mrbluebin@users.noreply.github.com>
2022-01-19 00:33:22 +0900

    Update ft_atoi_base.c

--- ex05/ft_atoi_base.c
+++ ex05/ft_atoi_base.c
@@ -14,121 +14,68 @@
 
 int g_num = 0;
 
-int	check_one(char *str)
-{
-	int	index;
-
-	index = 0;
-	while (str[index])
-	{
-		if (str[index] == '-' || str[index] == '+' || str[index] == ' ' || \
-				(str[index] >= 9 && str[index] <= 13))
-		{
-			return (1);
-		}
-		index++;
-	}
-	return (0);
-}
-
-int	check_same(char *base)
+int	check_base_error(char *base)
 {
 	int	i;
 	int	j;
-	int	index;
 
-	index = 0;
-	while (base[index])
-		index++;
 	i = 0;
-	j = 0;
-	while (base[i] != base[index])
+	while (base[i])
 	{
-		j = i + 1;
+		j = 0;
 		while (base[j])
 		{
-			if (base[i] == base[j])
-				return (1);
+			if (base[i] == base[j] && i != j)
+				return (0);
 			j++;
 		}
+		if (base[i] == ' ' || base[i] == '-' || base[i] == '+' || \
+			(base[i] >= 9 && base[i] <= 13))
+			return (0);
 		i++;
 	}
-	return (0);
-}
-
-int	ft_atoi(char *str, char *base, int baselen)
-{
-	int	index;
-	int	neg;
-	int	num;
-	int j;
-	int mult;
-
-	neg = 1;
-	index = 0;
-	num = 0;
-	mult = 1;
-	while ((str[index] == ' ' || (str[index] >= 9 && str[index] <= 13)))
-	{
-		index++;
-	}
-	while (str[index] == '-' || str[index] == '+')
-	{
-		if (str[index] == '-')
-			neg = -neg;
-		index++;
-	}
-	printf("%s",str);
-	while (str[index])
-	{
-		j = baselen - 1;
-		while (base[j] && j > -1)
-		{
-			if (str[index] == base[j])
-			{
-				num = num + (1 * mult * (baselen - j)) ;
-				break;
-			}
-			j--;
-		}
-		index++;
-		mult = mult * baselen;
-	}
-	return (num * neg);
-}
-
-int	get_base(char *base)
-{
-	int	len;
-
-	len = 0;
-	while (base[len])
-	{
-		if (base[len] == '-' || base[len] == '+')
-			return (0);
-		len++;
-	}
-	return (len);
-}
-
-void	re_print(int nbr, int len, char *base)
-{
-	if (nbr == 0)
-		return ;
-	re_print(nbr / len, len, base);
-	g_num = ((g_num * 10) + (base[nbr % len] - '0'));
+	return (i);
 }
 
 int	ft_atoi_base(char *str, char *base)
 {
 	int len;
-	len = get_base(base);
-	return (ft_atoi(str, base, len));	
+	int str_len;
+	int num;
+	int i;
+	int j;
+	int mult;
+
+	if (!(len = check_base_error(base)))
+		return (0);
+	str_len = 0;
+	while (str[str_len++]);
+	num = 0;
+	str_len--;
+	i = str_len - 1;
+	mult = 1;
+	while (i > -1)
+	{
+		j = 0;
+		while (base[j])
+		{
+			if (str[i] == base[j])
+			{
+				num = num + (mult * (j));
+				break;
+			}
+			j++;
+		}
+		i--;
+		mult *= len;
+	}
+	return (num);
+	
 }
 
 int	main(void)
 {
 	char a[50] = "2021";
 	char b[50] = "012";
-	printf("%d", ft_atoi_base(a, b));
+	printf("%d",ft_atoi_base(a,b));
 }
